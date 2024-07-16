8c49a27b2d2eaaa8fc13e2e664681a5f349432d4
elangreza14 <rezaelangerlangga14@gmail.com>
2024-07-17 03:57:07 +0700

    feat: add save logic and edit migration

--- plumbings/utils/respondJSON.go
+++ plumbings/utils/respondJSON.go
@@ -1,10 +1,22 @@
 package utils
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func FiberJSONWrap(c *fiber.Ctx, data any, count ...int) (err error) {
 	if len(count) == 1 {
 		return c.JSON(fiber.Map{"error": false, "data": data, "count": count[0]})
 	}
 	return c.JSON(fiber.Map{"error": false, "data": data})
+}
+
+func FiberJSONWrapWithStatusCreated(c *fiber.Ctx, data any, count ...int) (err error) {
+	if len(count) == 1 {
+		return c.Status(http.StatusCreated).JSON(fiber.Map{"error": false, "data": data, "count": count[0]})
+	}
+
+	return c.Status(http.StatusCreated).JSON(fiber.Map{"error": false, "data": data})
 }
