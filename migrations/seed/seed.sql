3a418df1a851bddaac682d64eb6a6cb27225930e
elangreza <rezaelangerlangga14@gmail.com>
2024-07-24 19:36:23 +0700

    fix: seed statuses

--- migrations/seed/seed.sql
+++ migrations/seed/seed.sql
@@ -6,10 +6,16 @@ VALUES
     ('Manager'),
     ('Sales');
 
-INSERT INTO clients (id, "name", logo_url)
+INSERT INTO
+    clients (id, "name", logo_url)
 VALUES
-    (1, 'client test', 'https://loremflickr.com/cache/resized/65535_53801147457_2a59fe2e69_300_300_nofilter.jpg');
+    (
+        1,
+        'client test',
+        'https://loremflickr.com/cache/resized/65535_53801147457_2a59fe2e69_300_300_nofilter.jpg'
+    );
 
-INSERT INTO statuses (code, "name", description)
+INSERT INTO
+    statuses (code, description)
 VALUES
-    ('DRAFT', 'draft', 'drafting mode');
+    ('DRAFT', 'drafting mode');
