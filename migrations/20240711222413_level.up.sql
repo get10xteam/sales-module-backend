7e0088ee25c3fbcaf396571988a4f01c79ea4c07
elangreza <rezaelangerlangga14@gmail.com>
2024-07-25 17:44:37 +0700

    fix: migration level and status

--- migrations/20240711222413_level.up.sql
+++ migrations/20240711222413_level.up.sql
@@ -5,6 +5,7 @@ create sequence if not exists level_sequence increment 100 start 100;
 
 create table levels(
     id bigint primary key default nextval('level_sequence'),
+    name text,
     create_ts timestamptz not null default now()
 );
 
--- migrations/20240711223538_status.up.sql
+++ migrations/20240711223538_status.up.sql
@@ -3,7 +3,6 @@ BEGIN
 
 create table statuses(
     code text primary key,
-    name text,
     description text,
     create_ts timestamptz not null default now()
 );
