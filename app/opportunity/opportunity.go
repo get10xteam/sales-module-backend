8db503b72b9adfa63ea49b4f8588c12657a1ec64
elangreza <rezaelangerlangga14@gmail.com>
2024-07-29 19:03:10 +0700

    fix: id in opportunity detail api

--- app/opportunity/opportunity.go
+++ app/opportunity/opportunity.go
@@ -328,7 +328,7 @@ func (s *opportunitiesSearchParams) GetData(ctx context.Context) ([]*Opportunity
 }
 
 func (s *opportunitiesSearchParams) GetSingle(ctx context.Context) (*Opportunity, error) {
-	s.q = s.q.Columns(s.columns()...).Limit(1)
+	s.q = s.q.Columns(s.columns()...).Where("o.id = ?", s.OpportunityID).Limit(1)
 	r, err := pgdb.QbQuery(ctx, s.q)
 	if err != nil {
 		return nil, err
